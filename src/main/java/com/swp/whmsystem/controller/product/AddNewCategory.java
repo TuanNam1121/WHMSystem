8443190ab0e85643a8780ef8a54e6c6ed7564ccc
NgDucAnh06 <ducanh06tb@gmail.com>
2026-05-27 20:59:35 +0700

    .

--- src/main/java/com/swp/whmsystem/controller/product/AddNewCategory.java
+++ src/main/java/com/swp/whmsystem/controller/product/AddNewCategory.java
@@ -1,7 +1,10 @@
 package com.swp.whmsystem.controller.product;
 
+import com.swp.whmsystem.dal.CategoryDAO;
 import com.swp.whmsystem.dal.RoleDAO;
+import com.swp.whmsystem.model.Category;
 import com.swp.whmsystem.model.Role;
+import com.swp.whmsystem.utils.InputStandization;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -22,7 +25,29 @@ public class AddNewCategory extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        String action = "new";
+        request.setAttribute("act", action);
 
+        String categoryName = InputStandization.validateName(request.getParameter("categoryName"));
+        String description = request.getParameter("description");
+
+        CategoryDAO categoryDAO = new CategoryDAO();
+        if (categoryDAO.getCategoryByName(categoryName) != null) {
+            String error = "Category name has already exsisted! Please input another one!";
+            request.getRequestDispatcher("CategoryDetail.jsp").forward(request, response);
+        }
+
+        Category category = new Category();
+        category.setCategoryName(categoryName);
+        category.setDescription(description);
+
+        if (categoryDAO.addNewCategory(category)) {
+            response.sendRedirect("ViewCategoryList");
+        } else {
+            String message = "Đã xảy ra lỗi!";
+            request.setAttribute("error", message);
+            request.getRequestDispatcher("CategoryDetail.jsp").forward(request, response);
+        }
     }
 
     @Override
--- src/main/java/com/swp/whmsystem/dal/CategoryDAO.java
+++ src/main/java/com/swp/whmsystem/dal/CategoryDAO.java
@@ -27,7 +27,7 @@ public class CategoryDAO {
         }
     }
 
-    public Category getCategoryName(String categoryName) {
+    public Category getCategoryByName(String categoryName) {
         String sql = "select * from categories where categoryName = ?";
         try (Connection connection = DBContext.getConnection()) {
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
@@ -41,6 +41,7 @@ public class CategoryDAO {
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
+        return null;
     }
 
     public Category mapResultSetToCategory(ResultSet rs) throws SQLException {
@@ -50,4 +51,32 @@ public class CategoryDAO {
         c.setDescription(rs.getString("description"));
         return c;
     }
+
+    public boolean addNewCategory(Category category) {
+        String sql = "insert into Categories(categoryName, description) values (?, ?)";
+        try (Connection connection = DBContext.getConnection()) {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1 ,category.getCategoryName());
+            ps.setString(2 ,category.getDescription());
+            return ps.executeUpdate() == 1;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return false;
+    }
+
+    public boolean updateCategory(Category category) {
+        String sql = "update Categories set categoryName = ?, description = ? where categoryId = ?";
+        try (Connection connection = DBContext.getConnection()) {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1, category.getCategoryName());
+            ps.setString(2, category.getDescription());
+            ps.setInt(3, category.getCategoryId()); // giả sử Category có trường categoryId
+            return ps.executeUpdate() == 1;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return false;
+    }
+
 }

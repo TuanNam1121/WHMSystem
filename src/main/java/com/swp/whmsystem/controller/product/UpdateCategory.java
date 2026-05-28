43c7ca6aaa35772b7d0157912a668b016471db84
NgDucAnh06 <ducanh06tb@gmail.com>
2026-05-28 09:56:01 +0700

    .

---
+++ src/main/java/com/swp/whmsystem/controller/product/UpdateCategory.java
@@ -0,0 +1,27 @@
+package com.swp.whmsystem.controller.product;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class UpdateCategory extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+}

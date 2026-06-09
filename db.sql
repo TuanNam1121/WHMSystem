6bcea5dcdd16cfe9a0f833c327fda9b52e1cfaf0
NgDucAnh06 <ducanh06tb@gmail.com>
2026-06-09 22:16:11 +0700

    Change status enum default value to 'NEW'

--- db.sql
+++ db.sql
@@ -111,7 +111,7 @@ CREATE TABLE `good_receipts` (
   `purchaserequestid` int(11) NOT NULL,
   `processedby` int(11) NOT NULL,
   `supplier_name` varchar(100) NOT NULL,
-  `status` enum('DRAFT','CANCELLED','SUBMITTED','COMPLETED','PENDING','REJECTED') DEFAULT 'DRAFT',
+  `status` enum('NEW', 'DRAFT','CANCELLED','SUBMITTED','COMPLETED','PENDING','REJECTED') DEFAULT 'NEW',
   `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime DEFAULT CURRENT_TIMESTAMP,
   `note` text,

dbc9e7a1f29df2a0b5de318d063f91aeb264978b
hnzgf <hung874864@gmail.com>
2026-06-30 14:27:44 +0700

    Brand, customer, sale order authorize

--- src/main/java/com/swp/whmsystem/controller/customer/UpdateCustomer.java
+++ src/main/java/com/swp/whmsystem/controller/customer/UpdateCustomer.java
@@ -7,6 +7,8 @@ package com.swp.whmsystem.controller.customer;
 
 import com.swp.whmsystem.dal.CustomerDAO;
 import com.swp.whmsystem.model.Customer;
+import com.swp.whmsystem.utils.AuthorizationUtils;
+import com.swp.whmsystem.utils.PermissionConstants;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
@@ -57,6 +59,11 @@ public class UpdateCustomer extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
+        
+        if (!AuthorizationUtils.checkAccess(request, response, PermissionConstants.UPDATE_CUSTOMER,
+                    "You are not authorized to update customer.")) {
+                return;
+            }
         String customerIdStr = request.getParameter("id");
         int customerId = Integer.parseInt(customerIdStr);
         CustomerDAO cd = new CustomerDAO();
